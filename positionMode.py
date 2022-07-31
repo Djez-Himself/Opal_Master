6ba6205875c603567cd9a64fabc3a19f350a4aa8
Ben <careydas.himself@gmail.com>
2022-07-31 16:36:03 +0200

    update

--- positionMode.py
+++ positionMode.py
@@ -7,7 +7,7 @@ import bybitconfig
 
 
 client = usdt_perpetual.HTTP("https://api-testnet.bybit.com",
-               api_key=bybitconfig.TESTNET_API_KEY, api_secret=bybitconfig.TESTNET_API_SECRET)
+               api_key=bybitconfig.BYBIT_API_KEY, api_secret=bybitconfig.BYBIT_API_SECRET)
 
 
 print(client.position_mode_switch(
